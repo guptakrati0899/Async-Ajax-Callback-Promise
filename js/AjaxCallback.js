4fdd431edc2d363b70046ee66568c051071b5b2d
KratiGupta <kratigupta8545@gmail.com>
2021-09-15 20:21:49 +0530

    Refactored code for viewing employee details

--- js/AjaxCallback.js
+++ js/AjaxCallback.js
@@ -2,42 +2,47 @@ let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 function showTime(){
     const date = new Date();
-    return date.getHours() + "Hrs: " + date.getMinutes() + "Mins: " + date.getSeconds() + "Secs: ";
+    return date.getHours() + "Hrs: " + date.getMinutes() + " Mins: " + date.getSeconds() + " Secs: ";
 }
 
 function makeAJAXCall(methodType, url, callback, async = true, data = null){
     let xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function(){
-        console.log(methodType + "State change called. Ready State: " + xhr.readyState + "Status: " + xhr.status);
-
+        // console.log(methodType + "State change called at: " +showTime() + " Ready State: " + xhr.readyState + " Status: " + xhr.status);
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200 || xhr.status === 201) {
+                callback(xhr.responseText);
+            } else if (xhr.status >= 400) {
+                console.log("handle 400 Client Error or 500 Server Error at: "+showTime());
+            }
+        }
     }
     xhr.open(methodType,url,true);
     xhr.send();
-    console.log(methodType + "request sent to server at: " + showTime());
+    console.log(methodType + " request sent to server at: " + showTime());
 }
 
-const getURL = "http://localhost:3000/employees/1";
+const getURL = "http://127.0.0.1:3000/employees/1";
 
 function getUserDetails(data){
-    console.log("Get user data at: " + showTime() + "Data: " + data);
+    console.log("Get user data at: " + showTime() + " Data: " + data);
 }
 
-
-makeAJAXCall("GET", getURL, true);
+makeAJAXCall("GET", getURL, getUserDetails, true);
 console.log("Made GET AJAX call to server at : " + showTime());
-
 
 const deleteURL = "http://localhost:3000/employees/4";
 function userDelete(data){
-    console.log("User Deleted " + showTime() + "Data: "+ data)
+    console.log("User Deleted: " + showTime() + " Data: "+ data)
 }
 
 makeAJAXCall("DELETE", deleteURL,userDelete,false);
 
-const postURL = "http://localhost:3000/employees"
-const emplData = {"name": "Akshara","salary":"800000"};
+const postURL = "http://localhost:3000/employees/"
+const emplData = {"name": "Akshara","salary":"700000"};
+
 function userAdded(data){
-    console.log("User Added:"  + showTime() + "Data: "+ data)
+    console.log("User Added: "  + showTime() + " Data: "+ data)
 }
 
-makeAJAXCall("POST",postURL,userAdded,true,emplData);
+makeAJAXCall("POST", postURL, userAdded, true, emplData);
